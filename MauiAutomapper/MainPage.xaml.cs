1e5c72a5f0640d5d64a31bbbc135bc7d22f472fb
Bulzan Sergiu <bulzan.sergiu@gmail.com>
2022-10-05 11:08:10 +0300

    Simplify example

--- MauiAutomapper/MainPage.xaml.cs
+++ MauiAutomapper/MainPage.xaml.cs
@@ -1,58 +1,28 @@
-﻿using MauiAutomapper.Models;
+﻿using AutoMapper;
 
 namespace MauiAutomapper;
 
 public partial class MainPage : ContentPage
 {
-    int count = 0;
+    private IMapper _mapper;
 
     public MainPage()
     {
         InitializeComponent();
 
-        DtoMapper.Initialize();
+        var config = new MapperConfiguration(cfg => cfg.CreateMap<MyClass, MyClassDto>().ReverseMap());
+        _mapper = config.CreateMapper();
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        var parent = new Parent()
+        var myClass = new MyClass()
         {
-            Name = "Andrew",
-            Children = new List<Child>()
-            {
-                new Child() {Age = 12 },
-                new Child() {Age = 12 },
-            }
+            Values = new List<int> { 1, 2, 3 }
         };
-
-        var parent2 = new Parent()
-        {
-            Name = "John",
-            Children = new List<Child>()
-            {
-                new Child() {Age = 9 },
-                new Child() {Age = 3 },
-            }
-        };
-
-        var children = new List<Child>()
-        {
-            new Child() {Age = 9 },
-            new Child() {Age = 3 },
-        };
-
-        //works fine on both iOS and Android
-        var result = DtoMapper.Map<List<ChildDto>>(children);
-        System.Diagnostics.Debug.WriteLine(result);
-
-        //fails on iOS. Cannot map the Children property.
-        //NOTE: ANDROID WORKS FINE!
-        var result1 = DtoMapper.Map<ParentDto>(parent);
-        System.Diagnostics.Debug.WriteLine(result1);
-
-        var parents = new List<Parent> { parent, parent2 };
-        var result2 = DtoMapper.Map<List<ParentDto>>(parents);
-        System.Diagnostics.Debug.WriteLine(result2);
+        // Works on Android, fails on iOS
+        var myClassDto = _mapper.Map<MyClassDto>(myClass);
+        System.Diagnostics.Debug.WriteLine(myClassDto);
     }
 }
 
